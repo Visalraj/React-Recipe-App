0519696db067684a9743fb8cc2600085c6b00aa9
V!sal <91835953+Visalraj@users.noreply.github.com>
2023-11-25 13:21:31 +0530

    Recipe-App

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-  // <React.StrictMode>
-  // </React.StrictMode>,
+   <React.StrictMode>
+        <App />
+   </React.StrictMode>,
 )
